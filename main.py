7edb853a2606198ba7084050645c5b5694444304
ridwanaryn <arrayyanr30@gmail.com>
2026-09-26 10:14:06 +0700

    add Player class and implement drawing functionality

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 import pygame
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
 from logger import log_state
+from player import Player
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     clock=pygame.time.Clock()
     dt=0.0
     while True:
@@ -13,6 +15,8 @@ def main():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
+        player.draw(screen)
+
         pygame.display.flip()
         dt = clock.tick(60) / 1000
         
