adf9d4bb9416b95fe53c1369e787371b0f9ce599
ridwanaryn <arrayyanr30@gmail.com>
2026-09-26 10:25:17 +0700

    implement player rotation and update methods for input handling

--- player.py
+++ player.py
@@ -1,6 +1,6 @@
 # player.py
 import pygame
-from constants import PLAYER_RADIUS, LINE_WIDTH
+from constants import LINE_WIDTH, PLAYER_RADIUS, PLAYER_TURN_SPEED
 from circleshape import CircleShape
 
 
@@ -8,6 +8,17 @@ class Player(CircleShape):
     def __init__(self, x, y):
         super().__init__(x, y, PLAYER_RADIUS)
         self.rotation = 0
+
+    def rotate(self, dt: float) -> None:
+        self.rotation += PLAYER_TURN_SPEED * dt
+
+    def update(self, dt: float) -> None:
+        keys = pygame.key.get_pressed()
+
+        if keys[pygame.K_a]:
+            self.rotate(-dt)
+        if keys[pygame.K_d]:
+            self.rotate(dt)
 
     def triangle(self) -> list[pygame.Vector2]:
         forward = pygame.Vector2(0, 1).rotate(self.rotation)
